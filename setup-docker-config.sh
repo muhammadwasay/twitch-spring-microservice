4445d906ebf3d239890f3de0f3d4ccc56c17bff8
Muhammad Wasay Khan <khan.wasay@gmail.com>
2020-06-09 01:55:26 +0400

    buildspec.yml update

--- setup-docker-config.sh
+++ setup-docker-config.sh
@@ -1,3 +1,6 @@
 mkdir -p ~/.docker
 > ~/.docker/config.json
 echo '{"credsStore": "ecr-login"}' | tee -a ~/.docker/config.json
+cat /root/.docker/config.json
+echo $PATH
+docker-credential-ecr-login -v
